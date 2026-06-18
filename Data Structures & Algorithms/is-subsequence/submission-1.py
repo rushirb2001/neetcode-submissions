c435c8232154fc63bbf6e55b215ab284a879f5bf
Rushir Bhavsar <167863043+rushirb2001@users.noreply.github.com>
2026-06-18 09:46:56 -0700

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # def rec(i, j):
+        #     if i == len(s):
+        #         return True
+        #     if j == len(t):
+        #         return False
+
+        #     if s[i] == t[j]:
+        #         return rec(i+1, j+1)
+            
+        #     return rec(i,j+1)
+
+        # return rec(0,0)
+
+        n, m = len(s), len(t)
+        memo = [[-1] * m for _ in range(n)]
+
+        def rec(i, j):
+            if i == n:
+                return True
+            if j == m:
+                return False
+
+            if memo[i][j] != -1:
+                return memo[i][j] == 1
+            
+            if s[i] == t[j]:
+                memo[i][j] = 1 if rec(i+1, j+1) else 0
+            else:
+                memo[i][j] = 1 if rec(i, j+1) else 0
+
+            return memo[i][j] == 1
+
+        return rec(0, 0)
+                    
